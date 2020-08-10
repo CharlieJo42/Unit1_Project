b196cf29f11e22d0fa25963ddf30cf986213ce32
Charlie Jo <charliejames1652@gmail.com>
2020-08-10 16:35:53 -0400

    added html and js

---
+++ main.js
@@ -0,0 +1,194 @@
+var safe1 = document.getElementById("1")
+
+safe1.addEventListener("click", safety1);
+function safety1 () {
+    document.getElementById("1").style.backgroundColor = "lightgreen";
+      document.getElementById("1").innerText = "Safe! (1)";
+}
+
+var pirate2 = document.getElementById("2")
+
+pirate2.addEventListener("click", arr2);
+function arr2 () {
+    document.getElementById("2").style.backgroundColor = "black";
+    alert("Arrghh you've been caught! Fork over the gold!");
+    location.reload();
+}
+
+var safe3 = document.getElementById("3")
+
+safe3.addEventListener("click", safety3);
+function safety3 () {
+    document.getElementById("3").style.backgroundColor = "lightgreen";
+    document.getElementById("3").innerText = "Safe! (0)";
+   }
+
+
+
+
+
+var pirate4 = document.getElementById("4")
+
+pirate4.addEventListener("click", arr4);
+function arr4 () {
+    document.getElementById("4").style.backgroundColor = "black";
+    alert("Time for you to walk the plank!");
+    location.reload();
+}
+
+
+var pirate5 = document.getElementById("5")
+
+pirate5.addEventListener("click", arr5);
+function arr5 () {
+    document.getElementById("5").style.backgroundColor = "black";
+    alert("Thought you could escape, huh?");
+    location.reload();
+}
+
+
+
+var safe6 = document.getElementById("6")
+
+safe6.addEventListener("click", safety6);
+function safety6 () {
+    document.getElementById("6").style.backgroundColor = "lightgreen";
+    document.getElementById("6").innerText = "Safe! (3)";
+}
+
+
+var safe7 = document.getElementById("7")
+
+safe7.addEventListener("click", safety7);
+function safety7 () {
+    document.getElementById("7").style.backgroundColor = "lightgreen";
+    document.getElementById("7").innerText = "Safe! (1)";
+}
+
+
+var pirate8 = document.getElementById("8")
+
+pirate8.addEventListener("click", arr8);
+function arr8 () {
+    document.getElementById("8").style.backgroundColor = "lightgreen";
+    document.getElementById("8").style.backgroundColor = "black";
+    alert("Not so fast there, cabin boy!");
+    location.reload();
+}
+
+
+
+var pirate9 = document.getElementById("9")
+
+pirate9.addEventListener("click", arr9);
+function arr9 () {
+    document.getElementById("9").style.backgroundColor = "black";
+    alert("Betcha didn't think the parrot would talk!");
+    location.reload();
+}
+
+
+
+var safe10 = document.getElementById("10")
+
+safe10.addEventListener("click", safety10);
+function safety10 () {
+    document.getElementById("10").style.backgroundColor = "lightgreen";
+    document.getElementById("10").innerText = "Safe! (1)";
+}
+
+
+var safe11 = document.getElementById("11")
+
+safe11.addEventListener("click", safety11);
+function safety11 () {
+    document.getElementById("11").style.backgroundColor = "lightgreen";
+    document.getElementById("11").innerText = "Safe! (2)";
+}
+
+
+
+var pirate12 = document.getElementById("12")
+
+pirate12.addEventListener("click", arr12);
+function arr12 () {
+    document.getElementById("12").style.backgroundColor = "black";
+    alert("I hope you can swim!");
+    location.reload();
+}
+
+
+var safe13 = document.getElementById("13")
+
+safe13.addEventListener("click", safety13);
+function safety13 () {
+    document.getElementById("13").style.backgroundColor = "lightgreen";
+    document.getElementById("13").innerText = "Safe! (1)";
+}
+
+
+var pirate14 = document.getElementById("14")
+
+pirate14.addEventListener("click", arr14);
+function arr14 () {
+    document.getElementById("14").style.backgroundColor = "black";
+    alert("There's not enough room on this island for all of us!");
+    location.reload();
+}
+
+
+var safe15 = document.getElementById("15")
+
+safe15.addEventListener("click", safety15);
+function safety15 () {
+    document.getElementById("15").style.backgroundColor = "lightgreen";
+    document.getElementById("15").innerText = "Safe! (1)";
+}
+
+
+var safe16 = document.getElementById("16")
+
+safe16.addEventListener("click", safety16);
+function safety16 () {
+    document.getElementById("16").style.backgroundColor = "lightgreen";
+    document.getElementById("16").innerText = "Safe! (2)";
+}
+
+
+var safe17 = document.getElementById("17")
+
+safe17.addEventListener("click", safety17);
+function safety17 () {
+    document.getElementById("17").style.backgroundColor = "lightgreen";
+    document.getElementById("17").innerText = "Safe! (2)";
+}
+
+
+var safe18 = document.getElementById("18")
+
+safe18.addEventListener("click", safety18);
+function safety18 () {
+    document.getElementById("18").style.backgroundColor = "lightgreen";
+    document.getElementById("18").innerText = "Safe! (1)";
+}
+
+
+var pirate19 = document.getElementById("19")
+
+pirate19.addEventListener("click", arr19);
+function arr19 () {
+    document.getElementById("19").style.backgroundColor = "black";
+    alert("You never stood a chance to escape!");
+    location.reload();
+}
+
+
+var pirate20 = document.getElementById("20")
+
+pirate20.addEventListener("click", arr20,);
+
+function arr20 () {
+    document.getElementById("20").style.backgroundColor = "black";
+    alert("I called dibs on your boots");
+    location.reload();
+}  
